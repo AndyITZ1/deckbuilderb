ea2625172e2d57940796e3546705db1518b0a5c2
Andy Tran <105564473+AndyITZ1@users.noreply.github.com>
2023-05-01 19:17:03 -0400

    First Commit Backend

---
+++ src/main/java/com/pokeview/PokeView/PokeViewApplication.java
@@ -0,0 +1,13 @@
+package com.pokeview.PokeView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokeview/PokeView/PokeViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokeview.PokeView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
